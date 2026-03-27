7be5e9b8b5572f11b58f9f8c3e6eade788451275
Emma-Pey <empeyrard3@gmail.com>
2026-03-27 17:29:21 +0100

    bd + page voyages

--- backend/services/voyages.service.js
+++ backend/services/voyages.service.js
@@ -1,5 +1,5 @@
-export async function createVoyage(server, username, title, startDate, endDate, description, lieu){
-    await server.pg.query("INSERT INTO voyages (username, title, startDate, endDate, description, lieu) VALUES ($1,$2,$3,$4, $5, $6)", [username, title, startDate, endDate, description, lieu])
+export async function createVoyage(server, username, startDate, endDate, description, lieu, createdAt){
+    await server.pg.query("INSERT INTO voyages (username, lieu, date_debut, date_fin, bio, created_at) VALUES ($1,$2,$3,$4, $5, $6)", [username, lieu, startDate, endDate, description, createdAt])
 }
 
 export async function getVoyages(server, type, date){
